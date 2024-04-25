03d1096867e14a63ffd0ab84ad98fea0d058438e
devendra_singh <rdev45450@gmail.com>
2024-04-25 13:57:54 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,11 +1,24 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+def isValid(s):
+    stack = []
+    pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for char in s:
+        if char in pairs:
+            stack.append(char)
+        else:
+            if not stack or pairs[stack.pop()] != char:
+                return False
+
+    return len(stack) == 0
+
+# Test cases
+print(isValid("()"))       # Output: True
+print(isValid("()[]{}"))   # Output: True
+print(isValid("(]"))       # Output: False
 
 
 
